0906f148c1f0741f2480dfc73ea0f00a52c780c6
Scot Morrison <SMorrison@spartaglobal.com>
2022-12-05 15:12:56 +0000

    initial commit of helloworldapp

--- Week1/Powershell/PigLatin/PigLatin.ps1
+++ Week1/Powershell/PigLatin/PigLatin.ps1
@@ -1,8 +1,8 @@
 ﻿$vowelsArray = "a","e", "i", "o", "u"
 $words = @()
 $newWords = @()
-$start = @()
 
+#Remove output file if present
 if(Get-Item -Path .\output.txt -ErrorAction Ignore)
 {
     Remove-Item '.\output.txt'
@@ -26,14 +26,16 @@ function Is-Vowel {
 $input = cat -Path .\input.txt
 foreach($line in $input)
 { 
+    #split each line into words
     $words += $line -split " "
     foreach($word in $words)
     {
-        $break = $true
         do{
+            #iteratively test each letter to see if it is a vowel
             $c = $word.Substring(0,1)
             if(Is-Vowel -L $c)
             {
+                #cover case where word starts with vowel
                 if($prefix.Length -eq 0)
                 {
                     $newWords += $prefix + $word + "yay"
@@ -42,6 +44,7 @@ foreach($line in $input)
                 {
                     $newWords += $word + $prefix + "ay"
                 }
+                #reset prefix for next word
                 $prefix = ""
                 Break
             }
@@ -49,7 +52,7 @@ foreach($line in $input)
             $word = $word.Substring(1, $word.Length-1)
         
         }
-        while($break)
+        while($true)
     }
 
 
