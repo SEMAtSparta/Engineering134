a6dc157182c5b8752387cef4ed9ab3d448ae82de
Scot Morrison <SMorrison@spartaglobal.com>
2022-12-06 11:12:02 +0000

    Added Pounds to Stone converter.

--- OperatorsApp/OperatorsApp/Program.cs
+++ OperatorsApp/OperatorsApp/Program.cs
@@ -15,15 +15,39 @@ namespace OperatorsApp
             int a = x++;
             int b = ++y;
 
-            for (int i = 1; i <= 5; ++i)
+            /*for (int i = 1; i <= 5; ++i)
             {
                 Console.WriteLine(i);
-            }
+            }*/
 
             var c = 5 / 2;      //truncated as 5 and 2 are type integers, so assigns c = 2.
             var d = 5.0 / 2;    //not truncated as 5.0 is type float, so assigns d = 2.5f
             double f = 5 / 2;   //still truncated before assignment happens, so assigns f = 2.
 
+            var mod = 5 % 2;    //assigns mod = 1;
+
+            LbsToStone(152);
         }
+
+        bool IsEven(int x)
+        {
+            return x % 2 == 0;
+        }
+
+        static void LbsToStone(int x)
+        {
+            int stone = x / 14;
+            int remainder = x % 14;
+            Console.WriteLine(x + "lbs is " + stone + " stone and " + remainder  + " ounces.");
+        }
+
+        /*static void MysteryCode()
+        {
+            int j = 5, k = 3, m = 4;
+
+            m += +j;
+        }*/
+
+        
       }
 }
