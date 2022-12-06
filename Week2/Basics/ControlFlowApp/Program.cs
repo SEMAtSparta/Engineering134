a4913e7acf0fc8ceb71d5af7ff48c742a3d5f122
Scot Morrison <SMorrison@spartaglobal.com>
2022-12-06 13:46:49 +0000

    Added switch statements to ControlFlowApp.

---
+++ Week2/Basics/ControlFlowApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ControlFlowApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mark = 35;
+            var grade = mark >= 65 ? (mark >= 85 ? "Distinction" : "Pass") : "Fail"; //? operator performs an inline if statement where: bool ? ifTrue : ifFalse;
+        }
+
+        public static string Priority(int level)
+        {
+            string priority = "Code ";
+            switch (level)
+            {
+                case 3:
+                    priority = priority + "Red";
+                    break;
+                case 2:
+                case 1:
+                    priority = priority + "Amber";
+                    break;
+                    
+                case 0:
+                    priority = priority + "Green";
+                    break;     
+                default:
+                    priority = "Error";
+                    break;
+            }
+            return priority;
+        }
+    }
+}
