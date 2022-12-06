6db74af16748ba50304278360daaa17bf0d2fd0c
Scot Morrison <SMorrison@spartaglobal.com>
2022-12-06 10:40:45 +0000

    Added examples for division truncation.

--- OperatorsApp/OperatorsApp/Program.cs
+++ OperatorsApp/OperatorsApp/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace OperatorsApp
+{
+      class Program
+      {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 5;
+
+            x++;
+            ++y;
+
+            int a = x++;
+            int b = ++y;
+
+            for (int i = 1; i <= 5; ++i)
+            {
+                Console.WriteLine(i);
+            }
+
+            var c = 5 / 2;      //truncated as 5 and 2 are type integers, so assigns c = 2.
+            var d = 5.0 / 2;    //not truncated as 5.0 is type float, so assigns d = 2.5f
+            double f = 5 / 2;   //still truncated before assignment happens, so assigns f = 2.
+
+        }
+      }
+}
