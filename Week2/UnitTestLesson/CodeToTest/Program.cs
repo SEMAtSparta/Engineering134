170bb93345232bb9328251c685116aeda7bf97ad
Scot Morrison <SMorrison@spartaglobal.com>
2022-12-07 17:12:44 +0000

    Added Unit test Lab assignment to CodeToTest/Program.cs and UnitTests.cs.

--- Week2/UnitTestLesson/CodeToTest/Program.cs
+++ Week2/UnitTestLesson/CodeToTest/Program.cs
@@ -27,4 +27,26 @@ public class Program
 
         return message; 
     }
+
+    public static string AvailableClassifications(int ageOfViewer)
+    {
+        string result;
+        if (ageOfViewer < 12)
+        {
+            result = "U & PG films are available.";
+        }
+        else if (ageOfViewer < 15)
+        {
+            result = "U, PG & 12 films are available.";
+        }
+        else if (ageOfViewer < 18)
+        {
+            result = "U, PG, 12 & 15 films are available.";
+        }
+        else
+        {
+            result = "All films are available.";
+        }
+        return result;
+    }
 }
--- Week2/UnitTestLesson/UnitTests/UnitTests.cs
+++ Week2/UnitTestLesson/UnitTests/UnitTests.cs
@@ -3,8 +3,7 @@ namespace UnitTests;
 
 public class Tests
 {
-    
-
+    #region GetGreeting Tests
     [TestCase(5)]
     [TestCase(6)]
     [TestCase(11)]
@@ -40,7 +39,29 @@ public class Tests
         string output = Program.GetGreeting(time);
         Assert.That(output, Is.EqualTo("Good evening!"));
     }
+    #endregion
 
+    #region Lab Tests
+    [TestCase(0, "U & PG films are available.")]//for range 0-11
+    [TestCase(5, "U & PG films are available.")]
+    [TestCase(11, "U & PG films are available.")]
+    [TestCase(12, "U, PG & 12 films are available.")]//for range 12-14
+    [TestCase(13, "U, PG & 12 films are available.")]
+    [TestCase(14, "U, PG & 12 films are available.")]
+    [TestCase(15, "U, PG, 12 & 15 films are available.")]//for range 15-17
+    [TestCase(16, "U, PG, 12 & 15 films are available.")]
+    [TestCase(17, "U, PG, 12 & 15 films are available.")]
+    [TestCase(18, "All films are available.")]//for range 18+
+    [TestCase(40, "All films are available.")]
+
+
+    public void Classification_Tests(int age, string availableFilms)
+    {
+        string result = Program.AvailableClassifications(age);
+        Assert.That(result, Is.EqualTo(availableFilms));
+    }
+    
+    #endregion
 
     /*Specimen Test
     [Test]
