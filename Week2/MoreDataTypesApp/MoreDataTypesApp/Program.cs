3379442e4bfdb244e54e03c5c6d7ee7c9f92c767
Scot Morrison <SMorrison@spartaglobal.com>
2022-12-09 11:23:33 +0000

    Added Arrays to MoreDataTypesApp.

--- Week2/MoreDataTypesApp/MoreDataTypesApp/Program.cs
+++ Week2/MoreDataTypesApp/MoreDataTypesApp/Program.cs
@@ -1,80 +1,97 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Text;
 
-namespace MoreDataTypesApp
+namespace MoreDataTypesApp;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var myString = " C# list fundamentals";
+
+        //StringInterpolation("Scot");
+        OneDArrays();
+
+    }
+    #region Strings
+
+
+    public static string StringExercise(string myString)
+    {
+        string newString = myString;
+
+        #region bootleg trim
+        /*while (newString[0] == ' ')
         {
-            var myString = " C# list fundamentals";
-            //Console.WriteLine(StringExercise(myString));
-            //Console.WriteLine(StringBuilderExercise(myString));
-            //Console.WriteLine("1." + "2." + "3." + "4." + "5." + "6." + "7." + "8." + "9." + 
-            //    "10." + "11." + "12." + "13." + "14." + "15." + "16." + "17." + "18." + "19." +
-            //    "20." + "21." + "22." + "23." + "24." + "25." + "26." + "27." + "28." + "29." +
-            //    "30." + "31." + "32." + "33." + "34." + "35." + "36." + "37." + "38." + "39." + 
-            //    "40." + "41.");
-            StringInterpolation("Scot");
-
+            newString = newString.Substring(1, newString.Length - 1);
         }
-
-        public static string StringExercise(string myString)
+        while (newString[newString.Length-1] == ' ')
         {
-            string newString = myString;
+            newString.Remove(newString.Length, 1);
+        }*/
+        #endregion
 
-            #region bootleg trim
-            /*while (newString[0] == ' ')
-            {
-                newString = newString.Substring(1, newString.Length - 1);
-            }
-            while (newString[newString.Length-1] == ' ')
-            {
-                newString.Remove(newString.Length, 1);
-            }*/
-            #endregion
+        
+        //newString = newString.Trim();
+        //newString = newString.ToUpper();
+        //newString = newString.Replace('L', '*');
+        //newString = newString.Replace('T', '*');
+        //this can all be shortened to 
 
-            
-            //newString = newString.Trim();
-            //newString = newString.ToUpper();
-            //newString = newString.Replace('L', '*');
-            //newString = newString.Replace('T', '*');
-            //this can all be shortened to 
+        newString = newString.Trim().ToUpper().Replace('L', '*').Replace('T', '*');
+        int position = newString.IndexOf('N');
+        newString = newString.Remove(position + 1);
+        return newString;
+    }
 
-            newString = newString.Trim().ToUpper().Replace('L', '*').Replace('T', '*');
-            int position = newString.IndexOf('N');
-            newString = newString.Remove(position + 1);
-            return newString;
-        }
+    public static string StringBuilderExercise(string myString)
+    {
+        var trimmedUpperString = myString.Trim().ToUpper();
+        int nPos = trimmedUpperString.IndexOf('N');
+        StringBuilder sb = new StringBuilder(trimmedUpperString);
+        sb.Replace('L', '*').Replace('T', '*').Remove(nPos + 1, sb.Length - nPos - 1);
+        return sb.ToString();
+    }
 
-        public static string StringBuilderExercise(string myString)
+    public static void StringInterpolation(string str)
+    {
+        Console.WriteLine("My name is :" + str + " Using +");
+        Console.WriteLine($"My name is :{str} using interpolation");
+
+        int num1 = 14;
+        int num2 = 7;
+
+
+        Console.WriteLine($"{num1} to the power of {num2} is {Math.Pow(num1, num2)}");
+        Console.WriteLine($"Log to base {num2} of {num1} is {Math.Log(num2,num1):0.####}");
+        Console.WriteLine($"That will be {num2/3.0:C} please");
+    }
+
+    //public static void ParsingStrings()
+    //{
+    //    Console.WriteLine("How many apples?");
+    //    string input = Console.ReadLine();
+    //    //int numApples = Int32.Parse(input);
+    //    var success = Int32.TryParse(input, out int parsedApples);
+    //}
+
+    #endregion
+
+    public static void OneDArrays()
+    {
+        int[] myIntArray = { 12, 23, 34, 45, 56 };
+        Array.Reverse(myIntArray);
+        foreach(var element in myIntArray)
         {
-            var trimmedUpperString = myString.Trim().ToUpper();
-            int nPos = trimmedUpperString.IndexOf('N');
-            StringBuilder sb = new StringBuilder(trimmedUpperString);
-            sb.Replace('L', '*').Replace('T', '*').Remove(nPos + 1, sb.Length - nPos - 1);
-            return sb.ToString();
+            Console.WriteLine(element);
         }
+    }
 
-        public static void StringInterpolation(string str)
-        {
-            Console.WriteLine("My name is :" + str + " Using +");
-            Console.WriteLine($"My name is :{str} using interpolation");
-
-            int num1 = 14;
-            int num2 = 7;
-
-
-            Console.WriteLine($"{num1} to the power of {num2} is {Math.Pow(num1, num2)}");
-            Console.WriteLine($"Log to base {num2} of {num1} is {Math.Log(num2,num1):0.####}");
-            Console.WriteLine($"That will be {num2/3.0:C} please");
-        }
-
-        public static void ParsingStrings()
-        {
-            Console.WriteLine("How many apples?");
-            string input = Console.ReadLine();
-            //int numApples = Int32.Parse(input);
-            var success = Int32.TryParse(input, out int parsedApples);
-        }
+    public static void MultiDArrays()
+    {
+        int[,] grid = new int[2, 4];
+        grid[0, 1] = 6;
+        grid[1, 0] = 8;
+        grid[1, 3] = 10;
     }
 }
