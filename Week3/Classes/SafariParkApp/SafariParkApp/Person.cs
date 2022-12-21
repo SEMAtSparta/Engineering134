15ba47b6b03a2a21891cf384e9761698d46d7412
Scot Morrison <SMorrison@spartaglobal.com>
2022-12-21 11:42:05 +0000

    Separated Person into its own class file.

---
+++ Week3/Classes/SafariParkApp/SafariParkApp/Person.cs
@@ -0,0 +1,55 @@
+﻿namespace SafariParkApp;
+
+
+public class Person : IMoveable
+{
+    public string FirstName;
+    public string LastName;
+    //private readonly string _eyecolour;
+    public int Age { get; set; } = 18;
+
+    private int _height = 0;
+    public int Height
+    {
+        get
+        {
+            return _height;
+        }
+        set
+        {
+            if (value < 0) _height = value;
+        }
+    }
+
+    public string FullName()
+    {
+        return FirstName + " " + LastName;
+    }
+
+    public Person(string firstName, string lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+    public Person(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()} {FirstName} {LastName}";
+    }
+
+    public string Move()
+    {
+        return $"{FirstName} takes a leisurely stroll.";
+    }
+
+    public string Move(int times)
+    {
+        return $"{FirstName} briskly moves {times} times.";
+    }
+}
